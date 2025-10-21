bd5c851a91e13647a07c2ed719903d81ce1910c7
neek-neek <neek2007@mail.ru>
2025-10-21 20:47:10 +0300

    config.py

---
+++ deepseek_python_20251021_42c7c7.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+CHANNEL_USERNAME = '@everyday_frames'  # Замените на ваш канал
+ADMIN_CHAT_ID = -1001234567890  # Замените на ID чата модераторов
